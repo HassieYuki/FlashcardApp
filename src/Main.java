1c11191c1942368830eb5359f18048940836ad3d
Yuki Hashimoto <y-hashimoto@keio.jp>
2021-11-19 18:51:09 +0900

    fix search error

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,20 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
+		// read all data from a text file
+		String textStoredPath = "C:\\Users\\g84oo\\GoogleDrive\\Eclipse\\Chinese\\recordText"; // folder path to the txt file
+		
+		
 		LaunchGUI launchPage = new LaunchGUI();
 		List<ArrayList<String>> listOfLists = launchPage.listOfLists;
 
-		// read all data from a text file
-		String storedPath = "C:\\Users\\g84oo\\GoogleDrive\\Eclipse\\Chinese\\recordText"; // folder path to the txt file
-
-		File folder  = new File(storedPath);
+		File folder  = new File(textStoredPath);
 		File[] listOfFiles = folder.listFiles();
 
 
 		for (int i = 0; i < listOfFiles.length; i++) {
 			System.out.println("Read " + listOfFiles[i].getName());
-			String newPath = storedPath + "\\" + listOfFiles[i].getName();
+			String newPath = textStoredPath + "\\" + listOfFiles[i].getName();
 			launchPage.textReadAdd(newPath,listOfLists);
 		}
 		
